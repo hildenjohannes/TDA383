7e4ad8172bc282b1e82a754bfc260931682f8afc
Johannes Hildén <hilden@ed3354-07.ed3354.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2017-01-25 15:16:24 +0100

    mid-development

--- trainspotting/src/Train.java
+++ trainspotting/src/Train.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.Semaphore;
 
 import TSim.CommandException; 
 import TSim.TSimInterface;
@@ -8,9 +9,11 @@ public class Train extends Thread {
 	
 	private int id;
 	private int speed;
+	private static Semaphore [] sem;
 	private TSimInterface tsi = TSimInterface.getInstance();
 	private List<Position> posList;
-	
+	private boolean headingNorth;
+		
 	private class Position {
 		
 		public int x;
@@ -39,26 +42,48 @@ public class Train extends Thread {
 		this.speed = speed;
 		this.posList = new ArrayList<Position>();
 		
-		posList.add(new Position(8,6));
-		posList.add(new Position(7,7));
-		posList.add(new Position(11,7));
-		posList.add(new Position(9,8));
-		posList.add(new Position(14,7));
-		posList.add(new Position(18,7));
-		posList.add(new Position(16,8));
-		posList.add(new Position(16,9));
-		posList.add(new Position(14,10));
-		posList.add(new Position(14,9));
-		posList.add(new Position(5,10));
-		posList.add(new Position(5,9));
-		posList.add(new Position(3,9));
-		posList.add(new Position(4,11));
-		posList.add(new Position(2,11));
-		posList.add(new Position(3,12));
+		if (id == 1) {
+			headingNorth = false;
+		} else {
+			headingNorth = true;
+		}
+		
+		if (sem == null) {
+			sem = new Semaphore [6];
+			for (int i = 0; i < 6; i++) {
+				sem[i] = new Semaphore(1);
+			}
+		}
+		
 		posList.add(new Position(14,3));
+		
+		posList.add(new Position(8,5));
 		posList.add(new Position(14,5));
+	
+		posList.add(new Position(6,7));
+		posList.add(new Position(10,7));
+		
+		posList.add(new Position(15,7));
+		posList.add(new Position(19,7));
+		
+		posList.add(new Position(9,8));
+		posList.add(new Position(16,8));
+		
+		posList.add(new Position(2,9));
+		posList.add(new Position(6,9));
+		posList.add(new Position(13,9));
+		posList.add(new Position(17,9));
+		
+		posList.add(new Position(5,10));
+		posList.add(new Position(14,10));
+		
+		posList.add(new Position(1,11));
+		posList.add(new Position(5,11));
 		posList.add(new Position(14,11));
+		
+		posList.add(new Position(3,13));
 		posList.add(new Position(14,13));
+		
 		
 		try {
 	      tsi.setSpeed(id,speed);
@@ -72,14 +97,104 @@ public class Train extends Thread {
 	public void run() {
 		while (true) {
 			try {
-				  System.out.println("run");
-				  SensorEvent sEvent = tsi.getSensor(1);
-				  
-				  System.out.println(sEvent.getXpos() + " " + sEvent.getYpos());
+				SensorEvent sEvent = tsi.getSensor(id);
+				 
+				if (sEvent.getStatus() == sEvent.ACTIVE) {
 				  			
-				  if (sEvent.getXpos() == 11 && sEvent.getYpos() == 7) {
-					  tsi.setSwitch(17, 7, tsi.SWITCH_RIGHT);
+				  if (sEvent.getXpos() == 15 && sEvent.getYpos() == 7) {
+					  if (headingNorth) {
+						  sem[2].release();
+					  } else {
+						  tsi.setSpeed(id,0);			
+						  sem[2].acquire();
+						  tsi.setSpeed(id, speed);
+						  tsi.setSwitch(17, 7, tsi.SWITCH_RIGHT);
+					  }
+					  
 				  } 
+				  
+				  if (sEvent.getXpos() == 17 && sEvent.getYpos() == 9) {
+					  if (headingNorth) {
+						  sem[3].release();
+					  } else {
+						  if (sem[3].tryAcquire()) {
+							  tsi.setSwitch(15, 9, tsi.SWITCH_RIGHT);
+						  } else {
+							  tsi.setSwitch(15, 9, tsi.SWITCH_LEFT);
+						  }
+					  }
+				
+				  } 
+				  
+				  if (sEvent.getXpos() == 14 && sEvent.getYpos() == 10) {
+					  if (headingNorth) {
+						  tsi.setSpeed(id,0);			
+						  sem[2].acquire();
+						  tsi.setSpeed(id, speed);
+						  tsi.setSwitch(15, 9, tsi.SWITCH_LEFT);
+					  } else {
+						  sem[2].release();
+					  }
+				  } 
+	
+				  if (sEvent.getXpos() == 2 && sEvent.getYpos() == 9) {
+					  if (headingNorth) {
+						  if (sem[3].tryAcquire()) {
+							  tsi.setSwitch(4, 9, tsi.SWITCH_LEFT);
+						  } else {
+							  tsi.setSwitch(4, 9, tsi.SWITCH_RIGHT);
+						  }
+					  } else {
+						  sem[3].release();
+					  }
+				  } 
+				  
+				  //blip
+				  if (sEvent.getXpos() == 6 && sEvent.getYpos() == 9) {
+					  if (headingNorth) {
+						  if (sem[3].tryAcquire()) {
+							  tsi.setSwitch(4, 9, tsi.SWITCH_LEFT);
+						  } else {
+							  tsi.setSwitch(4, 9, tsi.SWITCH_RIGHT);
+						  }
+					  } else {
+						  sem[3].release();
+					  }	
+				  } 
+				  
+				  //blop
+				  if (sEvent.getXpos() == 5 && sEvent.getYpos() == 10) {
+					  if (headingNorth) {
+						  if (sem[3].tryAcquire()) {
+							  tsi.setSwitch(4, 9, tsi.SWITCH_LEFT);
+						  } else {
+							  tsi.setSwitch(4, 9, tsi.SWITCH_RIGHT);
+						  }
+					  } else {
+						  sem[3].release();
+					  }
+				  } 
+				  
+				  
+				  if (sEvent.getXpos() == 13 && sEvent.getYpos() == 9) {
+					  
+					  if (headingNorth) {
+						  tsi.setSpeed(id,0);
+						  sem[2].acquire();
+						  tsi.setSpeed(id, speed);
+						  tsi.setSwitch(15, 9, tsi.SWITCH_RIGHT);
+					  } else {
+						  sem[2].release();
+					  }
+					  
+					  
+				  } 
+				  if ((sEvent.getXpos() == 18 && sEvent.getYpos() == 7) && headingNorth) {
+					  tsi.setSwitch(17, 7, tsi.SWITCH_RIGHT);
+					  sem[2].release();
+					  
+				  } 
+				}
 				  
 		//		  posList.contains(o)
 				  
@@ -94,4 +209,23 @@ public class Train extends Thread {
 			    }    
 		}
 	}
+	/**
+	private void hej(SensorEvent e, int xPos, int yPos) {
+		
+		if ((e.getXpos() == xPos && e.getYpos() == yPos) && !headingNorth) {
+			  System.out.println("blablablabl");
+			  tsi.setSpeed(id,0);
+			  System.out.println(sem[2].availablePermits());
+			  sem[2].acquire();
+			  System.out.println("då");
+			  tsi.setSpeed(id, speed);
+			  tsi.setSwitch(17, 7, tsi.SWITCH_RIGHT);
+		  } 
+		
+		
+		
+		
+		
+		
+	} */
 }
