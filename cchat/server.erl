9f31e72fe5e433bd207f10941d79e3b4936ddf8e
Johannes Hildén <hilden@ed3358-04.ed3358.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2017-02-24 11:52:23 +0100

    Added connect and disconnect

--- cchat/server.erl
+++ cchat/server.erl
@@ -18,6 +18,14 @@ initial_state(ServerName) ->
 %% {reply, Reply, NewState}, where Reply is the reply to be sent to the client
 %% and NewState is the new state of the server.
 
+deleteUser(List, Pid) ->
+    case List of
+    [] -> [];
+    [{P,_,_}|T] when P == Pid -> deleteUser(T,Pid);
+    [F|T] -> [F | deleteUser(T,Pid)]
+    end.
+
+
 handle(St, {nick,Nick}) ->
     Response = Nick,
     io:fwrite("Nick set to: ~p~n", [Response]),
@@ -26,4 +34,21 @@ handle(St, {nick,Nick}) ->
 handle(St, whoami) ->  
     Response = "japp",
     io:fwrite("Your nick: ~p~n", [Response]),
-    {reply, Response, St}.
+    {reply, Response, St};
+
+handle(St, {connect, Pid, Name}) ->
+    OldUsers = St#server_st.users,
+    NewUser = #user{pid = Pid, name = Name},
+    io:fwrite("Old: ~p~n", [OldUsers]),
+    io:fwrite("New: ~p~n", [NewUser]),
+    NewSt = St#server_st{users = [NewUser | OldUsers]},
+    {reply, ok, NewSt};
+
+handle(St, {disconnect, Pid}) ->
+    NewUsers = deleteUser(St#server_st.users, Pid),
+    NewSt = St#server_st{users = NewUsers},
+    {reply, ok, NewSt}.
+
+
+
+
