0c085abedc898eba42c34979f2ed4ff666d35e18
Tobias <toobew@users.noreply.github.com>
2017-02-28 19:01:03 +0100

    Removed channel, no longer in use. Server_st uses array of channel names instead

--- cchat/defs.hrl
+++ cchat/defs.hrl
@@ -7,10 +7,7 @@
 % This record defines the structure of a user matching a pid with a username and a potential channel.
 -record(user, {pid,name}).
 
--record(channel, {name,user_pids=[]}). %TODO: Maybe doesn't need user_pids
-
 % This record defines the structure of the server process.
-% Add whatever other fields you need.
 -record(server_st, {server,users=[],channels=[]}).
 
 -record(channel_st, {name,user_pids=[]}).
